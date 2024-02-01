e04b624dae969c72a46c411f0a0c70f4a73e99ee
Revolution825 <diarmuidoneill2003@gmail.com>
2024-02-01 09:30:55 +0000

    updatedVersion

--- Model2/DRIVER.java
+++ Model2/DRIVER.java
@@ -7,6 +7,8 @@ public class DRIVER
 {
     public static void main(String args[])
         {
+            DatabaseManager testDBM = new DatabaseManager();
 
+            testDBM.createEntry("accounts", "'testuser', 'TestUser123', 'User of Tests', 82,  'Lil Usey', 'England', 'The Big One', 'Y35 FKYU', 'hello@goodbye.sup', '0871234567'" );
         }
 }
