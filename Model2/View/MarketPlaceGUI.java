6400fcddc35bd50f343b88b53ca85e24e4df28bb
Revolution825 <diarmuidoneill2003@gmail.com>
2024-02-29 10:56:40 +0000

    MarketGUI

--- Model2/View/MarketPlaceGUI.java
+++ Model2/View/MarketPlaceGUI.java
@@ -1,10 +1,12 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class MarketPlaceGUI 
 {  
@@ -12,10 +14,15 @@ public class MarketPlaceGUI
     // Declarations
 
     private JFrame frame;
-    private JPanel buttonPanel;
+    private JPanel topPanel;
+    private JPanel loginSignupPanel;
+    private JPanel searchBarPanel;
+    private JPanel mainPanel;
     private JPanel imagePanel;
     private JButton loginButton;
     private JButton signUpButton;
+    private JButton searchButton;
+    private JTextField searchField;
 
     // Constructor
 
@@ -23,18 +30,31 @@ public class MarketPlaceGUI
         { 
             
             frame = new JFrame();                                                                       // Creates a JFrame object called frame
+            mainPanel = new JPanel(new BorderLayout());                                                 // Creates a JPanel instance called mainPanel                     
+            loginButton = new JButton("Login");                                                    // Creates a JButton instance called loginButton
+            signUpButton = new JButton("Sign Up");                                                 // Creates a JButton instance called signUpButton
+            searchButton = new JButton("Search");                                                  // Creates a JButton instance called searchButton
+            searchField = new JTextField(20);                                                   // Creates a JTextField instance called searchField
+            
+            topPanel = new JPanel(new GridLayout(1, 2));                                                // Creates a top panel using Grid layout (1 row, 2 columns) to hold the button panels
 
-            loginButton = new JButton("Login");                                                    // Creates a JButton instance called loginButton 
-            signUpButton = new JButton("Sign Up");                                                 //Creates a JButton instance called signUpButton
+            searchBarPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));                                         // Creates a panel using the Flow layout and positions it to the left
+            searchBarPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));       // Adds some borders to make things look nice
+            searchBarPanel.add(searchButton);
+            searchBarPanel.add(searchField);
+            
+            loginSignupPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));                                      // Creates a panel using the Flow layout and positions it to the right
+            loginSignupPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 0, 20));     // Adds some borders to make things look nice
+            loginSignupPanel.add(loginButton);
+            loginSignupPanel.add(signUpButton); 
 
-            buttonPanel = new JPanel();                                                                       // Creates a JPanel object called panel
-            buttonPanel.setPreferredSize(new Dimension(100,40));    
-            buttonPanel.setBorder(BorderFactory.createEmptyBorder(0, 1750, 10, 15));    // Creates a border on the panel, Setting the left border creates more space to bring it to the top right, will have to find a better way to do this
-            buttonPanel.setLayout(new GridLayout(1, 2));                                            // Changes the layout of the panel to the grid layout, with 0 rows and 1 column
-            buttonPanel.add(loginButton);
-            buttonPanel.add(signUpButton);
+            topPanel.add(searchBarPanel);
+            topPanel.add(loginSignupPanel);
 
-            frame.add(buttonPanel, BorderLayout.NORTH);                                                 // Adds the panel to the frame and centers it in the frame (Frame defaults to the border layout)
+            mainPanel.add(topPanel, BorderLayout.NORTH);                                                // Adds the top panel to the main panel using the border layout to position it to the top of the screen
+
+            frame.setMinimumSize(new Dimension(640, 480));                                 // Sets a minimum size for the frame
+            frame.add(mainPanel);                                                                       // Adds the panel to the frame and positions it to the centre (Frame defaults to the border layout)
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);                                       // Allows us to terminate the program upon closing the frame
             frame.setTitle("MarketPlace");                                                        // Sets title of frame
             frame.pack();                                                                               // Causes this Window to be sized to fit the preferred size and layouts of its subcomponents.
