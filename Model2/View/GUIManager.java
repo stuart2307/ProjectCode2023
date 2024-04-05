71970d69bdf3db6132c4ae56ca9b21499cf60977
Stuart2307 <c00284845@setu.ie>
2024-04-05 15:56:22 +0100

    commented GUI Manager

--- Model2/View/GUIManager.java
+++ Model2/View/GUIManager.java
@@ -4,7 +4,8 @@ import javax.swing.JPanel;
 
 public class GUIManager 
 {
-    private static JFrame frame = new JFrame("Crocodeal");
+    private static JFrame frame = new JFrame("Crocodeal");  //Creates the frame to hold all the panels
+    //INSTANTIATES AND INITIALISES ALL SCREENS
     private static MarketPlaceGUI marketplace = new MarketPlaceGUI();
     private static AccountPage accountpage = new AccountPage();
     private static Login login = new Login();
@@ -13,48 +14,48 @@ public class GUIManager
 
     public static void prepareManager()
         {
-            frame.add(marketplace);
-            frame.setMinimumSize(new Dimension(640, 480));
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.setTitle("MarketPlace");
-            frame.pack();
-            frame.setVisible(true); 
+            frame.add(marketplace);                                 //Adds the marketplace as the default panel
+            frame.setMinimumSize(new Dimension(640, 480));          //Sets a minimum size for the JFrame
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   //Makes it so that the program will terminate upon closing the frame
+            frame.setTitle("Crocodeal");                            //Sets the frame's title to Crocodeal
+            frame.pack();                                           //Sets the frame to adjust to the size of its components
+            frame.setVisible(true);                                 //Makes the frame visible
         }
         
 
     public static void changeMarketplace(JPanel switchFrom)
         {
-            switchFrom.setVisible(false);
-            frame.remove(switchFrom);
-            marketplace.setVisible(true);
-            frame.add(marketplace);
+            switchFrom.setVisible(false);                           //Sets the current screen's panel to be invisible
+            frame.remove(switchFrom);                               //Removes the current screen's panel from the frame
+            marketplace.setVisible(true);                           //Sets the marketplace's panel to be visible
+            frame.add(marketplace);                                 //Adds the marketplace's panel to the Frame
         }
         public static void changeLogin(JPanel switchFrom)
         {
-            switchFrom.setVisible(false);
-            frame.remove(switchFrom);
-            login.setVisible(true);
-            frame.add(login);
+            switchFrom.setVisible(false);                           //Sets the current screen's panel to be invisible
+            frame.remove(switchFrom);                               //Removes the current screen's panel from the frame
+            login.setVisible(true);                                 //Sets the login screen's panel to be visible
+            frame.add(login);                                       //Adds the login screen's panel to the Frame
         }
         public static void changeSignup(JPanel switchFrom)
         {
-            switchFrom.setVisible(false);
-            frame.remove(switchFrom);
-            signup.setVisible(true);
-            frame.add(signup);
+            switchFrom.setVisible(false);                           //Sets the current screen's panel to be invisible
+            frame.remove(switchFrom);                               //Removes the current screen's panel from the frame
+            signup.setVisible(true);                                //Sets the sign up screen's panel to be visible
+            frame.add(signup);                                      //Adds the sign up screen's panel to the Frame
         }
         public static void changeCreateAd(JPanel switchFrom)
         {
-            switchFrom.setVisible(false);
-            frame.remove(switchFrom);
-            createad.setVisible(true);
-            frame.add(createad);
+            switchFrom.setVisible(false);                           //Sets the current screen's panel to be invisible
+            frame.remove(switchFrom);                               //Removes the current screen's panel from the frame
+            createad.setVisible(true);                              //Sets the ad creation panel to be visible
+            frame.add(createad);                                    //Adds the ad creation panel to the Frame
         }
         public static void changeAccount(JPanel switchFrom)
         {
-            switchFrom.setVisible(false);
-            frame.remove(switchFrom);
-            accountpage.setVisible(true);
-            frame.add(accountpage);
+            switchFrom.setVisible(false);                           //Sets the current screen's panel to be invisible
+            frame.remove(switchFrom);                               //Removes the current screen's panel from the frame
+            accountpage.setVisible(true);                           //Sets the account view's panel to be visible
+            frame.add(accountpage);                                 //Adds the account view's panel to the Frame
         }
 }
