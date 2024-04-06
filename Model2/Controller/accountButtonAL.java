3c79dfa234c43b255d6519e0193806b12efda26d
Stuart2307 <c00284845@setu.ie>
2024-04-06 16:19:26 +0100

    Added an action listener class for the Account button

---
+++ Model2/Controller/accountButtonAL.java
@@ -0,0 +1,16 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
+
+public class accountButtonAL implements ActionListener 
+{
+    JPanel from;
+    public accountButtonAL(JPanel switchFrom)
+        {
+            from = switchFrom;
+        }
+    public void actionPerformed(ActionEvent accountButtonClicked)
+        {
+            GUIManager.changeAccount(from);
+        }    
+}
